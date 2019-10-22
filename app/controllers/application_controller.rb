d77f1e04ed70365215cf1e105519dd49bf79ecc8
Salvatore Santamaria <sam.santamaria@gmail.com>
2019-10-22 08:16:53 -0700

    Added Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    #video has protect_from_forgery with: :exception 
+    before_action :authenticate_user!
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index] #skip before action on welcome page (the index page)
   def index
   end
 end
